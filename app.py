045cbdd4319571ba2d58c475f79af3d2b5f9147d
Usama Mulla <116475880+M-Usama04@users.noreply.github.com>
2025-03-18 18:13:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import requests
 from bs4 import BeautifulSoup
-import PyMuPDF
 import fitz
 
 
